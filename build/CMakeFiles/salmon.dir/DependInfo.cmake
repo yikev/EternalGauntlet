b6eedbaadd59a291791b473929d1c01ecb25aabe
kookevk <kookevk@students.cs.ubc.ca>
2025-01-02 20:23:43 -0800

    added files

---
+++ build/CMakeFiles/salmon.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/ai_system.cpp" "CMakeFiles/salmon.dir/src/ai_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/ai_system.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/combat_stages.cpp" "CMakeFiles/salmon.dir/src/combat_stages.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/combat_stages.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/common.cpp" "CMakeFiles/salmon.dir/src/common.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/common.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/components.cpp" "CMakeFiles/salmon.dir/src/components.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/components.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/decision_tree.cpp" "CMakeFiles/salmon.dir/src/decision_tree.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/decision_tree.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/enemy_attacks.cpp" "CMakeFiles/salmon.dir/src/enemy_attacks.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/enemy_attacks.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/loot.cpp" "CMakeFiles/salmon.dir/src/loot.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/loot.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/main.cpp" "CMakeFiles/salmon.dir/src/main.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/main.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/physics_system.cpp" "CMakeFiles/salmon.dir/src/physics_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/physics_system.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/player_attacks.cpp" "CMakeFiles/salmon.dir/src/player_attacks.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/player_attacks.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/render_system.cpp" "CMakeFiles/salmon.dir/src/render_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/render_system.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/render_system_init.cpp" "CMakeFiles/salmon.dir/src/render_system_init.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/render_system_init.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/tiny_ecs.cpp" "CMakeFiles/salmon.dir/src/tiny_ecs.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/tiny_ecs.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/tiny_ecs_registry.cpp" "CMakeFiles/salmon.dir/src/tiny_ecs_registry.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/tiny_ecs_registry.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/world_init.cpp" "CMakeFiles/salmon.dir/src/world_init.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/world_init.cpp.o.d"
+  "/Users/kevinyi/Documents/EternalGauntlet/EternalGauntlet/src/world_system.cpp" "CMakeFiles/salmon.dir/src/world_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/world_system.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
